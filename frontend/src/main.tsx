ae95ada999e0fa422878118eb05211323a42ec04
MatrixYAS <rekeningfive@gmail.com>
2026-03-26 19:18:58 +0100

    Create main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+// frontend/src/main.tsx
+// Application Entry Point
+// Sets up React 18, StrictMode, and Theme Provider
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
